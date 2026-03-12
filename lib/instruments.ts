89477c97f0cbbd7285e7166dec6a928a03ef591f
Thomas Hadley <thomasehadley18@gmail.com>
2026-03-12 14:24:05 -0700

    expand futures instruments

--- lib/instruments.ts
+++ lib/instruments.ts
@@ -1,30 +1,49 @@
 export const INSTRUMENTS = {
-Forex:[
-"EURUSD","GBPUSD","USDJPY","USDCHF","USDCAD","AUDUSD","NZDUSD",
-"EURGBP","EURJPY","EURCHF","EURCAD","EURAUD",
-"GBPJPY","GBPCAD","GBPCHF",
-"AUDJPY","AUDCAD","AUDCHF",
-"NZDJPY","NZDCAD",
-"CADJPY","CHFJPY"
-],
+  Forex: [
+    "EURUSD","GBPUSD","USDJPY","USDCHF","USDCAD","AUDUSD","NZDUSD",
+    "EURGBP","EURJPY","EURCHF","EURCAD","EURAUD","EURNZD",
+    "GBPJPY","GBPCAD","GBPCHF","GBPAUD","GBPNZD",
+    "AUDJPY","AUDCAD","AUDCHF","AUDNZD",
+    "NZDJPY","NZDCAD","NZDCHF",
+    "CADJPY","CHFJPY"
+  ],
 
-Crypto:[
-"BTCUSD","ETHUSD","BNBUSD","SOLUSD","XRPUSD","ADAUSD","DOGEUSD"
-],
+  Crypto: [
+    "BTCUSD","ETHUSD","BNBUSD","SOLUSD","XRPUSD","ADAUSD","DOGEUSD",
+    "AVAXUSD","MATICUSD","LTCUSD","DOTUSD","LINKUSD","ATOMUSD",
+    "NEARUSD","FILUSD","ICPUSD","APTUSD","ARBUSD","OPUSD","UNIUSD"
+  ],
 
-Indices:[
-"NAS100","SPX500","US30","DAX40","FTSE100"
-],
+  Indices: [
+    "NAS100","SPX500","US30","DAX40","FTSE100",
+    "CAC40","NIKKEI","HSI","ASX200","IBEX35"
+  ],
 
-Commodities:[
-"XAUUSD","XAGUSD","WTI","BRENT","NATGAS"
-],
+  Commodities: [
+    "XAUUSD","XAGUSD","WTI","BRENT","NATGAS",
+    "COPPER","PLATINUM","PALLADIUM"
+  ],
 
-Futures:[
-"ES","NQ","YM","RTY","CL","GC","SI"
-],
+  Futures: [
+    "ES","MES","NQ","MNQ","YM","MYM","RTY","M2K",
 
-Stocks:[
-"AAPL","MSFT","NVDA","AMZN","GOOGL","META","TSLA"
-]
+    "CL","MCL","NG","QG","RB","HO","BZ",
+
+    "GC","MGC","SI","SIL","HG","MHG","PL","PA",
+
+    "ZB","UB","ZN","ZF","ZT","TN",
+
+    "6E","M6E","6B","M6B","6J","M6J","6A","M6A","6C","M6C","6N","M6N","6S",
+
+    "ZC","XC","ZW","XW","ZS","ZM","ZL","KE",
+
+    "LE","HE"
+  ],
+
+  Stocks: [
+    "AAPL","MSFT","NVDA","AMZN","GOOGL","META","TSLA",
+    "AMD","NFLX","BABA","SHOP","COIN","INTC","CSCO",
+    "DIS","PYPL","SQ","CRM","ORCL","ADBE",
+    "BA","UBER","LYFT","SNAP","ROKU","ZM"
+  ]
 }
